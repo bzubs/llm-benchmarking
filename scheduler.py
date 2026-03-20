9be703fc3560c7a3c72f5b05bbbe54af575e5707
bzubs <bzubs011@gmail.com>
2026-03-20 09:09:03 +0530

    ttmp: added GPU lifecycle and task assignment through roundrobin

---
+++ scheduler.py
@@ -0,0 +1,52 @@
+from schema import BenchmarkConfig, BenchTask
+from cluster import GPUCluster
+
+class GPUScheduler:
+    def __init__(self, cluster: GPUCluster):
+        self.cluster = cluster
+        self.rr_index = 0
+
+
+    def get_first_free_gpu(self):
+        for node in self.cluster: 
+            if node.gpu.status == "free":
+                return node
+        return None
+
+    def get_next_rr_node(self):
+        gpu_ids = self.cluster.get_gpu_ids()
+        gpu_id = gpu_ids[self.rr_index]
+
+        node = self.cluster.get_node(gpu_id)
+
+        self.rr_index = (self.rr_index + 1) % len(gpu_ids)
+        return node
+
+
+    def schedule_task(self, task: BenchTask):
+        node = self.get_first_free_gpu()
+
+        if node:
+            task.status = "assigned"
+            task.gpu_assigned = node.gpu.id
+            node.gpu.status = "busy"
+        else:
+            # Queue using round-robin
+            node = self.get_next_rr_node()
+            task.status = "queued"
+            task.gpu_assigned = node.gpu.id
+
+        self.cluster.append_task(node.gpu.id, task)
+
+
+def main():
+    cluster = GPUCluster(4)
+    scheduler = GPUScheduler(cluster)
+
+    tasks = [create_task(i) for i in range(10)]
+
+    for task in tasks:
+        scheduler.schedule_task(task)
+        print(f"Task {task.id} -> GPU {task.gpu_assigned} | status={task.status}")
+
+    print_cluster(cluster)
