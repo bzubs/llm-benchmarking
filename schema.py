e63493bb5f9b91340e433b96f78fc8d8b69c5a49
bzubs <bzubs011@gmail.com>
2026-03-22 00:04:13 +0530

    added BenchTaskResponse for API response

--- schema.py
+++ schema.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel, Field
-from typing import Optional, Literal, List
+from typing import Optional, Literal, List, Dict, Any
 
 
 class BenchmarkConfig(BaseModel):
+
     # mode and type both deprecated in v2
     benchmark_mode: str = "Online (Real-time Serving)"
     benchmark_type: Literal["serve"] = "serve"
@@ -10,33 +11,43 @@ class BenchmarkConfig(BaseModel):
     #user
     username : str = "none"
     
+    #model
     model_name: str
     dtype: Literal["auto", "fp16", "fp32", "bfloat16"] = "fp16"
     max_model_len : Optional[int] = 256
     quantization: Literal["int8", "int4", "fp8", "gptq", "awq", "none"] = "none"
+    
     # prompt
     dataset_name: Literal["random", "sharegpt", "custom"] = "random"
     input_len: Optional[int] = 128
     output_len: Optional[int] = 128
-    dataset_path: Optional[str] = None
+    
+
     # workload
     num_prompts: Optional[int] = 5
     max_concurrency: Optional[int] = 2
 
+    #backend
     backend: Optional[str] = "vllm"
     endpoint: Optional[str] = "/v1/completions"
-
-    #gpu_memory_util
-    gpu_memory_util : Optional[float] = 0.85
-
     host : str = "localhost"
     port : str = "8023"
 
+    #gpu
+    gpu_memory_util : Optional[float] = 0.85
+    n_gpus_required : int = 1
+
+    
 
 class GPU(BaseModel):
     id: int
     status : Literal["busy", "free"]
 
+class BenchResult(BaseModel):
+    config: BenchmarkConfig
+    returncode: int
+    runtime_sec: float
+    metrics: Dict[str, Any]
 
 
 class BenchTask(BaseModel):
@@ -44,9 +55,17 @@ class BenchTask(BaseModel):
     config : BenchmarkConfig
     status : Literal["init", "assigned", "running", "completed", "queued", "failed"]= "init"
     gpu_assigned : Optional[int] = None
+    result : Optional[BenchResult] = None
 
 
 
 class GPUNode(BaseModel):
     gpu: GPU
     queue: List[BenchTask] = Field(default_factory=list)
+
+
+class BenchTaskResponse(BaseModel):
+    id: int
+    gpu_assigned: Optional[int]
+    status: str
+    result : Optional[BenchResult] = None
