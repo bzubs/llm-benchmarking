075e26eda259b5e7bb571cd30a6c0982f877d67a
bzubs <bzubs011@gmail.com>
2026-03-29 23:17:31 +0530

    added req rate, gpu_type, error_msg for error propagation

--- schema.py
+++ schema.py
@@ -9,7 +9,7 @@ class BenchmarkConfig(BaseModel):
     benchmark_type: Literal["serve"] = "serve"
     
     #user
-    username : Optional[str] = None
+    username : str = "none"
     
     #model
     model_name: str
@@ -26,6 +26,7 @@ class BenchmarkConfig(BaseModel):
     # workload
     num_prompts: Optional[int] = 5
     max_concurrency: Optional[int] = 2
+    request_rate : Optional[int] = None
 
     #backend
     backend: Optional[str] = "vllm"
@@ -34,6 +35,7 @@ class BenchmarkConfig(BaseModel):
     port : str = "8000"
 
     #gpu
+    gpu_type : Literal["H100", "L40", "L4"] = "H100"
     gpu_memory_util : Optional[float] = 0.85
     n_gpus_required : int = 1
     dp_size : int = 1
@@ -49,25 +51,24 @@ class BenchResult(BaseModel):
     config: BenchmarkConfig
     returncode: int
     runtime_sec: float
-    metrics: Dict[str, Any]
+    metrics: Optional[dict] = None
+    error_msg : Optional[str] = ""
 
 
 class BenchTask(BaseModel):
     id : int
     config : BenchmarkConfig
     status : Literal["init", "assigned", "running", "completed", "queued", "failed"]= "init"
-    gpu_assigned : Optional[int] = None
+    gpu_assigned : Optional[List[int]] = None
     result : Optional[BenchResult] = None
 
 
 
 class GPUNode(BaseModel):
     gpu: GPU
-    queue: List[BenchTask] = Field(default_factory=list)
-
 
 class BenchTaskResponse(BaseModel):
     id: int
-    gpu_assigned: Optional[int]
+    gpu_assigned: Optional[List[int]]
     status: str
     result : Optional[BenchResult] = None
